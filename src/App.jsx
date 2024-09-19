51f2d812d64f436cd201df848a1c0d073c52e18e
Ritam_devs <ritamdevs2004@gmail.com>
2024-09-19 13:13:43 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import HowItWorks from "./components/HowItWorks"
 
 import * as Sentry from '@sentry/react'
 import Features from "./components/Features"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Model />
       <Features/>
       <HowItWorks/>
+      <Footer/>
     </main>
   )
 }
